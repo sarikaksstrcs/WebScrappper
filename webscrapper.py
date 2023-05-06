af707fb85cf1b3cb44159e4b27882775c215343c
K S Sarika <sarika.ks.official@gmail.com>
2023-05-06 11:37:03 +0530

    Scrapped Malayalam Lyrics

--- webscrapper.py
+++ webscrapper.py
@@ -2,18 +2,29 @@ import requests
 from bs4 import BeautifulSoup
 
 # URL of the webpage containing the poem lines
-url = 'https://www.poetryfoundation.org/poems/46473/the-road-not-taken'
+url = 'https://www.malayalachalachithram.com/song.php?i=13178'
 
+#url = input("Enter Url")
 # Send a GET request to the webpage URL and retrieve the HTML content
 response = requests.get(url)
 html_content = response.content
 
 # Use Beautiful Soup to parse the HTML content and extract the poem lines
 soup = BeautifulSoup(html_content, 'html.parser')
-poem_lines = [line.text.strip() for line in soup.find_all('div', class_='c-feature-bd')]
+#print(soup)
+#print(soup.prettify())
+
+full_lyrics = soup.find_all('table',id = "tbllyrics")
+#print(full_lyrics)
+for lyrics in full_lyrics:
+    l = lyrics.find_all('td')
+    print(l[1].text)
+#print(soup.get_text())
+
+poem_lines = [line.text.strip() for line in soup.find_all('table',id = "tbllyrics")]
 
 # Print the extracted poem lines
-print(poem_lines)
+#print(poem_lines)
 #This code should extract all the poem lines from the webpage and print them to the console. However, the specific HTML elements and class names used to extract the poem lines may vary depending on the webpage you're scraping.
 
 
